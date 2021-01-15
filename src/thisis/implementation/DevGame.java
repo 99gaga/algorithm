d4e076e606eed10fee649dbfa82a20325b9fc9b3
yjs <wotlr789@naver.com>
2021-01-15 22:27:03 +0900

    게임 개발

---
+++ src/thisis/implementation/DevGame.java
@@ -0,0 +1,11 @@
+package thisis.implementation;
+
+import java.util.Scanner;
+
+//게임 개발
+public class DevGame {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
