3c0a2a5e07b5d31b6c1708f5072b3aecf57421cd
yjs <wotlr789@naver.com>
2021-05-18 09:01:36 +0900

    [백준 11726] 2*n 타일링

---
+++ src/baekjon/dp/boj11726.java
@@ -0,0 +1,23 @@
+package baekjon.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 2*n 타일링
+ */
+public class boj11726 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] d = new int[n+1];
+        d[0] = 1;
+        d[1] = 1;
+        for (int i=2; i<=n; i++) {
+            d[i] = (d[i-2] + d[i-1]) % 10007;
+        }
+        System.out.println(d[n]);
+    }
+}
