103f3c9a1834b6621116219ffb623eca21f909ba
yjs <wotlr789@naver.com>
2021-06-05 23:57:07 +0900

    [프로그래머스] 짝지어 제거하기

---
+++ src/programmers/짝지어제거하기.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+public class 짝지어제거하기 {
+
+    @Test
+    void test() {
+        Assertions.assertEquals(1, solution("baabaa"));
+        Assertions.assertEquals(0, solution("cdcd"));
+    }
+
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
