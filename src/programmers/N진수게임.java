8900c4b95488e6f67ca0dd25b6425d31ebb18571
jaesika <wotlr789@naver.com>
2022-01-08 23:13:43 +0900

    [프로그래머스] N진수 게임
    * n진수의 숫자를 순서대로 부른다. ex) 2진수의 경우, 0110111000... 여기서 한 숫자씩만 말해야 한다. m명중 순서가 p인 튜브가 자신이 앞으로 t개까지 무엇을 말해야하는지 구해야한다.
    
    convertNumber 메서드에서 숫자를 차례대로 변환하여 temp 문자열에 t * m 을 넘을 때까지 이어 붙힌다.
    이어붙힌 문자열을 가지고 p번부터 m씩 더하며 answer에 이어 붙힌다.

---
+++ src/programmers/N진수게임.java
@@ -0,0 +1,50 @@
+package programmers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.util.Scanner;
+
+public class N진수게임 {
+
+    @Test
+    void test() {
+        Assertions.assertEquals("0111", solution(2, 4, 2, 1));
+        Assertions.assertEquals("02468ACE11111111", solution(16, 16, 2, 1));
+        Assertions.assertEquals("13579BDF01234567", solution(16, 16, 2, 2));
+    }
+
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder answer = new StringBuilder();
+        StringBuilder temp = new StringBuilder("0");
+
+        int number = 0;
+        while (temp.length() - 1 < t * m) {
+            temp.append(convertNumber(number, n));
+            number++;
+        }
+
+        String tempStr = temp.toString();
+        for (int i = p - 1; i <= (t - 1) * m + p; i += m) {
+            answer.append(tempStr.charAt(i));
+        }
+
+        return answer.toString();
+    }
+
+    public String convertNumber(int number, int n) {
+        StringBuilder sb = new StringBuilder();
+        while (number > 0) {
+            int r = number % n;
+            if (r >= 10) {
+                char s = (char) ('A' + r - 10);
+                sb.insert(0, s);
+            } else {
+                sb.insert(0, r);
+            }
+            number /= n;
+        }
+        return sb.toString();
+    }
+}
